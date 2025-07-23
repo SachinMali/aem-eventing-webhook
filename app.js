5c59f60b70a0aa2f74871f16f4d7031ebf0c66c0
Sachin Mali <sachinpree@gmail.com>
2025-07-23 19:10:09 -0400

    dded 'unsafe-eval' to allow jQuery templates while keeping other security protections:

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(helmet({
     directives: {
       defaultSrc: ["'self'"],
       styleSrc: ["'self'", "'unsafe-inline'", "https://maxcdn.bootstrapcdn.com"],
-      scriptSrc: ["'self'", "'unsafe-inline'", "https://code.jquery.com", "https://ajax.aspnetcdn.com", "https://maxcdn.bootstrapcdn.com"],
+      scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'", "https://code.jquery.com", "https://ajax.aspnetcdn.com", "https://maxcdn.bootstrapcdn.com"],
       imgSrc: ["'self'", "data:", "https:"],
       connectSrc: ["'self'", "wss:", "ws:"]
     }
